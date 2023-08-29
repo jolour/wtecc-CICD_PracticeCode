5ef336133ef804e225fb6f60d6e17ba64a6fb7a3
jolour <jorflourenco@gmail.com>
2023-08-29 23:07:01 +0100

    change files

---
+++ labs/03_use_tekton_catalog/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#tkn task ls
+#tkn pipeline ls
+
+tkn hub install task git-clone
+
+kubectl apply -f tasks.yaml
+kubectl apply -f pvc.yaml
+kubectl apply -f pipeline.yaml
+
+tkn pipeline start cd-pipeline \
+    -p repo-url="https://github.com/ibm-developer-skills-network/wtecc-CICD_PracticeCode.git" \
+    -p branch="main" \
+    -w name=pipeline-workspace,claimName=pipelinerun-pvc \
+    --showlog
+
+tkn pipelinerun ls
+tkn pipelinerun logs --last
